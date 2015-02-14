12214adb7e7a61a455f22b3c8fe75f37d0a34aba
SAKATA Sinji <contact@sinjis-view.mydns.jp>
2015-02-14 22:18:11 +0900

    5.37 generatePrototype

--- src/codegen.cpp
+++ src/codegen.cpp
@@ -42,3 +42,28 @@ bool CodeGen::generateTranslationUnit(Tra slationUnitAST &tunit, std::string nam
   }
   return true;
 }
+
+Function *CodeGen::generatePrototype(PrototypeAST *proto, Module *mod){
+  Function *func = mod->getFunction(proto->getName());
+  if(func){
+    if(func->arg_size() == proto->getParamNum() && func->empty()){
+      return func;
+    }
+    else{
+      fprintf(stderr, "error::function %s id redefined", proto->getName().c_str());
+      return NULL;
+    }
+  }
+
+  std::vector<Type*> int_types(proto->getParamNum(), Type::getInt32Ty(getGlobalContext()));
+  FunctionType *func_type = FunctionType::get(Type::getInt32Ty(getGlobalContext()), int_type, false);
+  func = Functin::Create(func_type, Function::ExternalLinkage, proto->getName(), mod);
+
+  Function::arg_iterator arg_iter = func->arg_begin();
+  for(int i = 0; i< proto->getParamNum(); i++){
+    arg_iter->setName(proto->getParamName(i).append("_arg"));
+    arg_iter++;
+  }
+
+  return func;
+}
