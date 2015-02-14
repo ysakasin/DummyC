d8741daf1818e0111e9b0aec5c00263dd6fc05e2
SAKATA Sinji <contact@sinjis-view.mydns.jp>
2015-02-14 22:09:22 +0900

    5.36 generateTranslationUnit

--- src/codegen.cpp
+++ src/codegen.cpp
@@ -15,3 +15,30 @@ Module &CodeGen::getModule(){
     return *(new Module("null", getGlobalContext()));
   }
 }
+
+bool CodeGen::generateTranslationUnit(Tra slationUnitAST &tunit, std::string name){
+  Mod = new Module(name, getClobalContext());
+
+  for(int i = 0; ; i++){
+    PrototypeAST *proto = trunit.getPrototype(i);
+    if(!proto){
+      break;
+    }
+    else if(!generatePrototype(proto, Mod)){
+      SAFE_DELETE(Mod);
+      return false;
+    }
+  }
+
+  for(int i = 0; ; i++){
+    FunctionAST *func = trunit.getFunction(i);
+    if(!func){
+      break;
+    }
+    else if(!generateDunctionDefinition(func, Mod)){
+      SAFE_DELETE(Mod);
+      return false;
+    }
+  }
+  return true;
+}
